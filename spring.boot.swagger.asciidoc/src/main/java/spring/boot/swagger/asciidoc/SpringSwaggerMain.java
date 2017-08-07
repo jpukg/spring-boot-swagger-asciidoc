9681f5f273504eee1cf1a4db290443234882f35c
orbartal <orbartal@gmail.com>
2017-08-07 18:34:39 -0400

    Add project basic files for spring boot hello world.

---
+++ spring.boot.swagger.asciidoc/src/main/java/spring/boot/swagger/asciidoc/SpringSwaggerMain.java
@@ -0,0 +1,12 @@
+package spring.boot.swagger.asciidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerMain.class, args);
+	}
+}
